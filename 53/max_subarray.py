57c07d605836f3cc94786e6d8e1960ae6c413ef5
Gischenato <gischenato@gmail.com>
2024-04-14 23:07:34 -0300

    53. Maximum Subarray

---
+++ 53/max_subarray.py
@@ -0,0 +1,10 @@
+def maxSubArray(self, nums: list[int]) -> int:
+    max_sum = nums[0]
+    curr_sum = 0
+
+    for n in nums:
+        curr_sum = max(curr_sum, 0)
+        curr_sum += n
+        max_sum = max(max_sum, curr_sum)
+
+    return max_sum
